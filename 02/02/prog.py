13841b9c4fd8cac7701b764d089ec632e32d221b
Cedric Trost <cedric.trost@helpsystems.com>
2018-12-05 16:39:02 +0100

    fix broke stuff.

--- 02/02/prog.py
+++ 02/02/prog.py
@@ -6,16 +6,14 @@ with open('../input', 'r') as f:
 def diff1(id1, id2):
     for i, chars in enumerate(zip(id1, id2)):
         if chars[0] != chars[1]:
-            if (id1[:i] + id1[i+1:]) == (id2[:i] + id2[i+1:]):
-                return id1
-            else:
-                return False
+            return (id1[:i] + id1[i+1:]) == (id2[:i] + id2[i+1:]), id1[:i] + id1[i+1:]
+    return False, None
     
 for i, container_id in enumerate(ids):
     for other_container_id in ids[i:]:
-        found_it = diff1(container_id, other_container_id)
+        found_it, answer = diff1(container_id, other_container_id)
         if found_it:
-            print(found_it)
+            print(answer)
             exit(0)
 
 
