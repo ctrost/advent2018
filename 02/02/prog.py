f987192d72c1d1f0395dfd2d7c0c2aa48aca1aea
Cedric Trost <cedric.trost@helpsystems.com>
2018-12-05 16:35:17 +0100

    Fixed 02/02, it was ugly

--- 02/02/prog.py
+++ 02/02/prog.py
@@ -1,20 +1,21 @@
-with open('../input', 'r') as f:
+from sys import exit
+
+with open('../input', 'r') as f: 
     ids = [line.strip() for line in f.readlines()]
 
-def diff(id1, id2):
-    for i in range(len(id1)):
-        if id1[i] != id2[i]:
-            id1 = id1[:i] + id1[i+1:]
-            id2 = id2[:i] + id2[i+1:]
-            if id1 == id2: 
+def diff1(id1, id2):
+    for i, chars in enumerate(zip(id1, id2)):
+        if chars[0] != chars[1]:
+            if (id1[:i] + id1[i+1:]) == (id2[:i] + id2[i+1:]):
                 return id1
             else:
                 return False
     
 for i, container_id in enumerate(ids):
     for other_container_id in ids[i:]:
-        if diff(container_id, other_container_id):
-            print(diff(container_id, other_container_id))
-            break
+        found_it = diff1(container_id, other_container_id)
+        if found_it:
+            print(found_it)
+            exit(0)
 
 
